ba7501818f965165a2d3ee241eb50959f4f37208
Pedro <pedrogarrido971@gmail.com>
2024-04-18 15:00:06 -0300

    Terminé landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    InicioComponent,
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
