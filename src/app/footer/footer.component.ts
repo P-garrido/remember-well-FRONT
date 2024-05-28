57d984e987e7b1a138bdcca58665aa1efefaaa4f
Pedro <pedrogarrido971@gmail.com>
2024-05-28 15:23:51 -0300

    Terminé...

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor() {
+
+  }
+
+  emailForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 }
